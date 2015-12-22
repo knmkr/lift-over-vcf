01ffab2e2eccc7b02d0e29defa1ef049b964615d
Kensuke Numakura <knmkr3gma@gmail.com>
2015-12-22 15:33:32 +0900

    fix bin and chain path

--- install.sh
+++ install.sh
@@ -17,8 +17,15 @@ cd ..
 mkdir -p chain
 cd chain
 
+# from hg18/b36
 wget -c http://hgdownload-test.cse.ucsc.edu/goldenPath/hg18/liftOver/hg18ToHg19.over.chain.gz
 wget -c http://hgdownload-test.cse.ucsc.edu/goldenPath/hg18/liftOver/hg18ToHg38.over.chain.gz
+
+# from hg19/b37
 wget -c http://hgdownload-test.cse.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz
+wget -c http://hgdownload-test.cse.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg18.over.chain.gz
+
+# from hg38/b38
+wget -c http://hgdownload-test.cse.ucsc.edu/goldenPath/hg38/liftOver/hg38ToHg19.over.chain.gz
 
 cd ..
--- lift_over.py
+++ lift_over.py
@@ -10,8 +10,8 @@ import subprocess
 
 
 def lift_over(chrom, pos, strand, chain):
-    lift_over_exec = os.path.join('bin', 'liftOver')
-    map_chain = os.path.join('chain', chain + '.over.chain.gz')
+    lift_over_exec = os.path.join(os.path.dirname(__file__), 'bin', 'liftOver')
+    map_chain = os.path.join(os.path.dirname(__file__), 'chain', chain + '.over.chain.gz')
 
     with tempfile.NamedTemporaryFile() as query:
         q = '\t'.join(['chr' + chrom if not chrom.startswith('chr') else chrom,  # chrom in .chain startswith `chr`
@@ -27,6 +27,7 @@ def lift_over(chrom, pos, strand, chain):
         with tempfile.NamedTemporaryFile() as mapped, tempfile.NamedTemporaryFile() as unmapped:
             # $ ./liftOver <oldFile> <map.chain> <newFile> <unMapped>
             cmd = [lift_over_exec, query.name, map_chain, mapped.name, unmapped.name]
+            print >>sys.stderr, '[DEBUG]', cmd
             subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
 
             mapped.seek(0)
@@ -41,22 +42,25 @@ def reverse_complement(x):
 def _main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--chain', choices=['hg18ToHg19', 'hg18ToHg38', 'hg19ToHg38'], required=True)
+    parser.add_argument('--vcf', action='store_true')
     args = parser.parse_args()
 
+
     for line in sys.stdin:
-        if line.startswith('#'):
-            print line,
-        else:
-            record = line.strip().split('\t')
-            new = lift_over(record[0],
-                            int(record[1]),  # 1-based pos
-                            '+',             # vcf is always forward to reference
-                            args.chain)
-            if new:
-                new_chrom, new_pos, new_strand = new
-                ref = record[3] if new_strand == '+' else reverse_complement(record[3])
-                alt = record[4] if new_strand == '+' else reverse_complement(record[4])
-                print '\t'.join([new_chrom, new_pos, record[2], ref, alt] + record[5:])
+        if args.vcf:
+            if line.startswith('#'):
+                print line,
+            else:
+                record = line.strip().split('\t')
+                new = lift_over(record[0],
+                                int(record[1]),  # 1-based pos
+                                '+',             # vcf is always forward to reference
+                                args.chain)
+                if new:
+                    new_chrom, new_pos, new_strand = new
+                    ref = record[3] if new_strand == '+' else reverse_complement(record[3])
+                    alt = record[4] if new_strand == '+' else reverse_complement(record[4])
+                    print '\t'.join([new_chrom, new_pos, record[2], ref, alt] + record[5:])
 
 
 if __name__ == '__main__':
